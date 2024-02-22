d4693882d9305d61924b8aa7c762e2c2aa73aeb1
Mathunilah <baskaranmathunilah0129@gmail.com>
2024-02-22 14:11:21 +0530

    images

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Login from './auth/Login';
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ src/auth/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Vector from '../../images/Vector.svg'
+
+export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle login logic
+  };
+
+  return (
+    <div className="relative flex w-full h-screen">
+      {/* Image positioned at the bottom-left corner */}
+      <img
+        src="../../../images/Vector.svg"
+        className="absolute bottom-0 left-0"
+        alt="Background Image"
+      />
+      <div className=''>
+        <img src={Vector} />
+      </div>
+      <div className="w-full flex items-center justify-center">
+        <div className=" w-11/12 max-w-[500px] px-10 py-20 rounded-3xl bg-white border border-gray-100 relative">
+          <h1 className="text-5xl text-center font-semibold">Admin Login</h1>
+          <div className="mt-8">
+            <div className="flex flex-col">
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Enter Username"
+                required 
+              />
+            </div>
+            <div className="flex flex-col mt-4">
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                placeholder="Enter Password"
+                required
+                type={'password'}
+              />
+            </div>
+            <div className="mt-8 flex justify-between items-center">
+              <div>
+                <input type="checkbox" id="remember" />
+                <label className="ml-2 font-medium text-base" for="remember">
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <div className="mt-8 flex flex-col items-center justify-center gap-y-4">
+              <button
+                onClick={handleLogin}
+                className=" hover:bg-[#296d9a] w-40 py-4 bg-[#215E88] rounded-full text-white font-bold text-lg"
+              >
+                Login
+              </button>
+            </div>
+            <div className="mt-8 flex justify-center items-center">
+              <button className="font-medium text-base text-black">
+                Forgot password ?
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
